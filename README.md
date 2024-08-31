25204bc54077b42ee581635aab4743668f73a40c
Sagar <jussagar1@gmail.com>
2024-08-31 23:07:01 +0545

    Project initiated

---
+++ README.md
@@ -0,0 +1,4 @@
+# LayoutDimension-CSS
+This project is about playing with CSS properties mainly related to height, width and centering the div. 
+
+The project will also test with flexbox and grid layout in the future. 
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <div className='text title'>
+        THIS IS A CONTAINER
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
